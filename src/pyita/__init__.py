5e1bf7a991ece17d92a92b5d691bd8c3d2e6a6d5
hal9000cc <hal@hal9000.cc>
2026-02-11 18:04:37 +0300

    export IndicatorsResult

--- src/pyita/__init__.py
+++ src/pyita/__init__.py
@@ -14,6 +14,7 @@ import re
 from pathlib import Path
 
 from .quotes import Quotes
+from .indicator_result import IndicatorResult
 from .exceptions import (
     PyTAException,
     PyTAExceptionIndicatorNotFound,
@@ -51,6 +52,7 @@ def _get_version():
 __version__ = _get_version()
 __all__ = [
     'Quotes',
+    'IndicatorResult',
     'PyTAException',
     'PyTAExceptionIndicatorNotFound',
     'PyTAExceptionBadParameterValue',
